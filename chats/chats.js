adfffa5fe0203429724d06cfdc4ecddf649f95f5
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-05-20 17:47:26 +0530

    some new change in the ui of chats page

--- chats/chats.js
+++ chats/chats.js
@@ -1,10 +1,10 @@
 let token = localStorage.getItem('token');
+let userName = parseJwt(token);
 async function submitMessage(e) {
     try {
         e.preventDefault();
         let messages = e.target.messages.value;
         let data = await axios.post('http://localhost:3000/chats/saveMessage', { messages: messages }, { headers: { Authorization: token } });
-        console.log(data);
     } catch (error) {
         showErrorMessage(error);
     }
@@ -22,4 +22,38 @@ function showErrorMessage(error) {
             errorParent.removeChild(document.getElementById('errorChild'));
         }
     }, 4000);
+}
+//user refreshes the page it get fire the function 
+window.addEventListener('DOMContentLoaded', async () => {
+    try {
+        let data = await axios.get('http://localhost:3000/chats/getMessage', { headers: { Authorization: token } });
+        if (data.data.length > 0) {
+            for (var i = 0; i < data.data.length; i++) {
+                addingListInTheScreen(data.data[i]);
+            }
+        }
+    } catch (error) {
+        showErrorMessage(error);
+    }
+})
+// adding list
+function addingListInTheScreen(data) {
+    let listsParent = document.getElementById('listOfChats');
+    let name;
+    if (userName.name === data.user.name) {
+        name = 'You ';
+    } else {
+        name = data.user.name;
+    }
+    listsParent.innerHTML += `<li>${name}: ${data.messages}</li>`;
+}
+//decoding the token
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
 }
